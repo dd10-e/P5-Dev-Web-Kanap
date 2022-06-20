f569899d5d532a5a04d00705fa763a237c93bf8c
mcrestey <kius@live.fr>
2022-06-20 10:02:28 +0200

    Modification ou suppression d'un produit
    Modification ou suppression d'un produit dans la page panier. Non fonctionnel,

--- front/html/js/cart.js
+++ front/html/js/cart.js
@@ -11,28 +11,55 @@ if (items.length === 0) {
             products.forEach((product) => {
                 display(product)
             })
-            /* console.log('===')
-             displayTotal(products)
-             console.log('===')
-             products.forEach(product => {
-                 listenForQtyChange(product)
-                 ListenForDelection(product)
-             })
- */
+            displayTotal(products)
+            products.forEach(product => {
+                //listenForQtyChange(product)
+                listenForDelection(product)
+            })
+
 
 
         })
 }
 
-function listenForQtyChange(product) {
-    document.querySelector(`article.cart__item[data-id="${product.id}"] .itemQuantity`).addEventListener('input', () => {
 
+
+
+function buildCompleteList(data, itemsInCart) {
+    let list = [];
+
+    itemsInCart.forEach(item => {
+        let product = data.find(el => el._id === item.id);
+        let pro = { ...product }
+        pro.qty = item.qty;
+        pro.color = item.color;
+
+        list.push(pro)
     })
+    return list;
+}
+
+function countTotalProduct(products) {
+    let totalQty = 0;
+
+    products.forEach(product => {
+        totalQty += Number(product.qty);
+    })
+    return totalQty
+}
+
+function countTotalPrice(products) {
+    let totalPrice = 0;
+
+    products.forEach(product => {
+        totalPrice += (Number(product.price) * Number(product.qty))
+    })
+    return totalPrice
 }
 
 ///affiche les informations du produit sur la page en html
 function display(product) {
-    document.getElementById("cart__items").innerHTML =
+    document.getElementById("cart__items").innerHTML +=
         `<article class="cart__item" data-id="${product.id}" data-color="${product.color}">
         <div class="cart__item__img">
         <img src="${product.imageUrl}" alt="${product.altTxt}"></img>
@@ -58,40 +85,70 @@ function display(product) {
 
 }
 
-function buildCompleteList(data, itemsInCart) {
-    let list = [];
-
-    itemsInCart.forEach(item => {
-        let product = data.find(el => el._id === item.id);
-        let pro = { ...product }
-        pro.qty = item.qty;
-        pro.color = item.color;
-
-        list.push(pro)
-    })
-    return list;
-}
-
-function countTotalProduct(product) {
-    let totalQty = O;
-
-    products.forEach(product => {
-        totalQty += Number(product.qty);
-    })
-    return totalQty
-}
-
-function countTotalPrice(product) {
-    let totalPrice = O;
-
-    products.forEach(product => {
-        totalPrice += (Number(product.price) * Number(product.qty))
-    })
-    return totalPrice
-}
-
-
-
 function displayTotal(products) {
-    document.querySelector('h1').innerText = countTotalProduct(products)
+    document.getElementById("totalQuantity").innerText = countTotalProduct(products)
+    document.getElementById("totalPrice").innerText = countTotalPrice(products)
 }
+
+
+function listenForDelection(product) {
+    let deleteItem = document.querySelectorAll(".deleteItem");
+    console.log(deleteItem);
+
+    deleteItem.forEach((delete) => {
+        delete.addEventListener("click", () => {
+            console.log(delete);
+            let totalProductRemove = product.length;
+            console.log(totalProductRemove);
+
+            if (totalProductRemove == 1) {
+                return (
+                    localStorage.removeItem("product"),
+                    console.log("remove tout le panier")
+                );
+            }
+            else {
+                XXXXX
+            }
+        }
+
+
+/*
+function listenForQtyChange(product) {
+    consolelog("===")
+    let newProductQty = product.qty
+    document.querySelectorAll(".itemQuantity").addEventListener("change", (newProductQty) => {
+        if (product.qty < newProductQty) {
+            alert('Un produit a été ajouté')
+            product.qty++
+            localStorage.cart = JSON.stringify(cart);
+            displayTotal();
+        }
+        else {
+            alert('Un produit a été supprimé')
+            product.qty--
+            localStorage.cart = JSON.stringify(cart);
+            displayTotal();
+        }
+    })
+}
+
+
+function listenForQtyChange(product) {
+    document.querySelectorAll(".itemQuantity").addEventListener("change", () => {
+        const adjustQty = document.getElementById('itemQuantity').value;
+        if (product.qty < product.adjustQty && product.color === cart.dataset.color && product.id === product._id) {
+            alert('Un produit a été ajouté')
+            product.qty++
+            localStorage.cart = JSON.stringify(cart);
+            displayTotal();
+        }
+        else {
+            alert('Un produit a été supprimé')
+            product.qty--
+            localStorage.cart = JSON.stringify(cart);
+            displayTotal();
+        }
+    })
+}
+*/
